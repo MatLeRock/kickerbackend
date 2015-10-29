7c94d38e0193a91579e1b324e3d02278563edb86
Matthias Rock <matthias.rock@nacamar.de>
2015-10-29 14:55:46 +0100

    - changed doctrine.global.php to PostgreSQL

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -4,7 +4,7 @@ return array(
     'doctrine' => array(
         'connection' => array(
             'orm_default' => array(
-                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOPgSql\Driver',
                 'params' => array(
                     'host' => 'localhost',
 //                        'port' => '3306',
